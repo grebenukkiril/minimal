0cb0edcc38239180b82bcaedb3831a4855dbc498
Гребенюк Кіріл <grebenukkiril06@gmail.com>
2024-03-03 14:32:24 +0200

    останній коміт

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("burger").addEventListener("click", function(){
         document.querySelector("header").classList.toggle("open")
-        // const style = document.createElement('style');
-        // style.textContent = 'body { overflow: hidden; }';
-        // document.head.appendChild(style);
     })
 })
